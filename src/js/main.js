c50f51731baa5c64113a37059c477a5f5153d3d9
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2024-11-19 00:14:49 +0300

    com

--- src/js/main.js
+++ src/js/main.js
@@ -124,11 +124,17 @@ closeBtn.addEventListener("click", () => {
     closeBtn.classList.add("invisible");
 })
 
-filterBtn.addEventListener("click", disableScroll);
-backBtn.addEventListener("click", enableScroll);
-closeBtn.addEventListener("click", enableScroll);
+filterBtn.addEventListener("click", disScroll);
+backBtn.addEventListener("click", enScroll);
+closeBtn.addEventListener("click", enScroll);
 
+function disScroll() {
+    document.querySelector("body").classList.add("body_noscroll")
+}
 
+function enScroll() {
+    document.querySelector("body").classList.remove("body_noscroll")
+}
 
 /*слайдер*/
 
@@ -149,34 +155,4 @@ slider.addEventListener("input", () => {
     fillSliderColor();
 })
 
-//drap and drop
 
-function dropHandler(ev) {
-    console.log("File(s) dropped");
-  
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-  
-    if (ev.dataTransfer.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      [...ev.dataTransfer.items].forEach((item, i) => {
-        // If dropped items aren't files, reject them
-        if (item.kind === "file") {
-          const file = item.getAsFile();
-          console.log(`… file[${i}].name = ${file.name}`);
-        }
-      });
-    } else {
-      // Use DataTransfer interface to access the file(s)
-      [...ev.dataTransfer.files].forEach((file, i) => {
-        console.log(`… file[${i}].name = ${file.name}`);
-      });
-    }
-  }
-  
-  function dragOverHandler(ev) {
-    console.log("File(s) in drop zone");
-  
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-  }
